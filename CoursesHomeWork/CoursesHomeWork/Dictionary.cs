b03e5a7c68010e4dc98664b2d78adee84d495099
vladislavpushkin <vladpushkin098@gmail.com>
2018-07-02 10:37:35 +0300

    Enum implementation removed
    Enum implementation removed

--- CoursesHomeWork/CoursesHomeWork/Dictionary.cs
+++ CoursesHomeWork/CoursesHomeWork/Dictionary.cs
@@ -1,16 +1,8 @@
-﻿using System;
-
-namespace HomeWorkTask1
+﻿namespace HomeWorkTask1
 {
     internal class Dictionary
     {
-        public Languages Language { get; set; }
+        public string Language { get; set; }
         public string Text { get; set; }
-
-        public override string ToString()
-        {
-            return $"{Language.ToString()}:";
-        }
-
     }
 }
--- CoursesHomeWork/CoursesHomeWork/Languages.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HomeWorkTask1
-{
-    internal enum Languages
-    {
-        English=1,
-        French =2
-
-    }
-}
